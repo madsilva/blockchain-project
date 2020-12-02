d68a79868556b50a002f87d13cec1aefd720c92a
madsilva <mad.silva.ic@gmail.com>
2020-12-01 20:30:22 -0600

    add to form

--- truffle/client/src/ContractInfoForm.js
+++ truffle/client/src/ContractInfoForm.js
@@ -1,66 +1,88 @@
-import React from 'react';
-import {Form, FormGroup, Input, FormText, Button,InputGroup,InputGroupText,InputGroupAddon,Label} from 'reactstrap';
-var contract = require("@truffle/contract");
+import React from 'react'
+import {Form, FormGroup, Input, Button, Label} from 'reactstrap'
 
+const contract = require("@truffle/contract")
 const AffiliateContractJSON = require('./contracts/AffiliateContract.json')
+const AffiliateSubcontractJSON = require('./contracts/AffiliateSubcontract.json')
 
 class ContractInfoForm extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.web3 = props.web3
+    this.affiliateContract = contract({abi: AffiliateContractJSON.abi})
+    this.affiliateSubcontract = contract({abi: AffiliateSubcontractJSON.abi})
+    this.affiliateContract.setProvider(this.web3.currentProvider)
+    this.affiliateSubcontract.setProvider(this.web3.currentProvider)
     this.state = {
-      // main contract address
-      mainContractAddress: ' '
-    };
+      mainContractAddress: ' ',
+      subcontractAddress: ' ',
+      subcontractIndex: 0
+    }
     this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  handleChange(e, name) {
-    this.state[name] = e.target.value;
-    this.setState(this.state);
-    
   }
 
   handleInputChange(event) {
-    const {name, value} = event.target;
-    console.log(name);
-    console.log(value);
-    this.state[name]=value
+    const {name, value} = event.target
+    this.state[name] = value
     this.setState(this.state)
-    //this.setState({[name]: value}), () => (console.log(this.state));
-    //if((name=='totalFunds') || (name=='nSubcontracts')){
-    //  this.state[SCfunds] = this.state[totalFunds]/this.state[nSubcontracts];
- //} 
-    console.log(this.state)
   }
 
-  async handleSubmit(event) {
-    // all form input values need to be validated by this point
-    const accounts = await this.web3.eth.getAccounts()
-    var AffiliateContract = contract({abi: AffiliateContractJSON.abi, bytecode: AffiliateContractJSON.bytecode});
-    AffiliateContract.setProvider(this.web3.currentProvider);
-    //var newContract = await AffiliateContract.new()
+  handleMainContractInfo(event) {
+
+  }
+
+  handleMainContractParams(event) {
+
+  }
+
+  handleSubcontractIndex(event) {
+
+  }
+
+  handleSubcontractInfo(event) {
+
   }
 
   render() {
     return(<React.Fragment>
-    <Form id="inputForm" onSubmit={ this.handleSubmit }>
-      <h2>Get info about existing contracts</h2>
-      <FormGroup>
-        <Label for="mainContractAddress">Main contract address</Label>
-        <Input
-          type="text"
-          name = "mainContractAddress"
-          defaultValue={this.state.mainContractAddress}
-          onChange={this.handleInputChange}
-          id="mainContractAddress"
-          //placeholder="buyerId" 
-        />
-      </FormGroup>
-      
-      <Button color="primary" type='submit' form='inputForm'>Submit</Button>
-    </Form>
+      <Form id="inputForm">
+        <h2>Get info about existing contracts or subcontracts</h2>
+        <h3>You can do this while using any account in Metamask</h3>
+        <FormGroup>
+          <Label for="mainContractAddress">Main contract address</Label>
+          <Input
+            type="text"
+            name = "mainContractAddress"
+            defaultValue={this.state.mainContractAddress}
+            onChange={this.handleInputChange}
+            id="mainContractAddress"
+          />
+          <Button color="primary" form='inputForm' onClick={ this.handleMainContractInfo }>Get main contract info</Button>
+          <br />
+          <Button color="primary" form='inputForm' onClick={ this.handleMainContractParams }>Get main contract parameters</Button>
+          <br />
+          <Label for="subcontractIndex">Subcontract index</Label>
+          <Input
+            type="text"
+            name = "subcontractIndex"
+            defaultValue={this.state.subcontractIndex}
+            onChange={this.handleInputChange}
+            id="subcontractIndex"
+          />
+          <Button color="primary" form='inputForm' onClick={ this.handleSubcontractIndex }>Get subcontract address</Button>
+        </FormGroup>
+        <FormGroup>
+          <Label for="subcontractAddress">Subcontract address</Label>
+          <Input
+            type="text"
+            name = "subcontractAddress"
+            defaultValue={this.state.subcontractAddress}
+            onChange={this.handleInputChange}
+            id="subcontractAddress"
+          />
+          <Button color="primary" form='inputForm' onClick={ this.handleSubcontractInfo }>Get subcontract info</Button>
+        </FormGroup> 
+      </Form>
     </React.Fragment>)
   }
 }
