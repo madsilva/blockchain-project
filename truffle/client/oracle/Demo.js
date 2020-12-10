8beb780477996468eb2086007a1a4c697d7699c3
attackTheChocha <conor-henry@uiowa.edu>
2020-12-09 18:36:37 -0600

    main.py

--- truffle/client/oracle/Demo.js
+++ truffle/client/oracle/Demo.js
@@ -10,30 +10,16 @@ const affiliateContract = contract({abi: AffiliateContractJSON.abi, bytecode: Af
 
 const affiliateSubcontract = contract({abi: AffiliateSubcontractJSON.abi})
 
-async function getCallerContract (web3js, ownerAddress) {
-  const networkId = await web3js.eth.net.getId()
-  const oracleAddress =  OracleJSON.networks[networkId].address
-  var mainContract = new web3js.eth.Contract(AffiliateContractJSON.abi, options={gas: 100000000000})
-  const accounts = await web3js.eth.getAccounts()
-  const affiliate = accounts[1]
-
-  const arguments = [affiliate, oracleAddress, 4, 600, 300, 1000000, 1000000, 10]
-
-  const gas = await mainContract.deploy({data: AffiliateContractJSON.bytecode, arguments: arguments}).estimateGas({value: 2000000});
-  mainContract = await mainContract.deploy(
-    {data: AffiliateContractJSON.bytecode, arguments: arguments})
-    .send({from: ownerAddress, value: 2000000, gas: gas + 100})
-  const receipt = await mainContract.methods.getMainContractStateInfo().send({from: ownerAddress, gas: 2000000})
-
-  const subcontract = await new web3js.eth.Contract(AffiliateSubcontractJSON.abi, receipt.events.GetMainContractStateInfoEvent.returnValues.currentSubcontract)
-  return { subcontract, mainContract }
-}
+const nSubcontracts = 4;
+const sliceLength = 60;
+const SGPlength = 30;
+const commission = 10;
 
 async function filterEvents (callerContract,index) {
   try{
 
     callerContract.CurrentTotalUpdatedEvent()
-  .on('data', event => console.log('SC' + index + ' * New CurrentTotalUpdated event. affiliateTotal: ' + event.currentTotal));
+  .on('data', event => console.log('SC' + index + ' * New CurrentTotalUpdated event. affiliateTotal: ' + event.returnValues.currentTotal));
   //callerContract.events.CurrentTotalUpdatedEvent({ filter: { } }, async (err, event) => {
   //  if (err) { 
   //    console.error('Error on event', err)
@@ -45,7 +31,7 @@ async function filterEvents (callerContract,index) {
 }
 }
 
-async function init (nSC, scduration, sgpduration, scStake, incentiveFee, commissionRate ) {
+async function init (nSC, scDuration, SGPDuration, scStake, incentiveFee, commissionRate ) {
   const web3js = await getWeb3.getWeb3()
   const accounts = await web3js.eth.getAccounts()
   const ownerAddr = accounts[0]
@@ -55,8 +41,7 @@ async function init (nSC, scduration, sgpduration, scStake, incentiveFee, commis
   affiliateContract.setProvider(web3js.currentProvider)
   affiliateSubcontract.setProvider(web3js.currentProvider)
   const txVal = String(Number(incentiveFee) + Number(scStake))
-  const scDuration = scduration * 60
-  const SGPDuration = sgpduration * 60
+
   
   const mainContract = await affiliateContract.new(
     affiliateAddr,
@@ -84,18 +69,103 @@ async function init (nSC, scduration, sgpduration, scStake, incentiveFee, commis
   return { sc0, mainContract, ownerAddr, affiliateAddr, oracleAddr, web3js } 
 }
 
+//agp=300
+
+
+async function updateCurrentTotal(scIndex,caller){
+  console.log(caller+" Called updateCurrentTotal on SC"+scIndex)
+  await contracts[""+scIndex].updateCurrentTotal({from: addreses[caller],gas: 2000000})
+}
+
+async function checkEarnings(scIndex,caller){
+  console.log(caller+" Called checked earnings on SC"+scIndex)
+  await contracts[""+scIndex].currentTotal.call() 
+}
+
+async function createNextSubContract(sliceIndex,caller){
+  console.log(caller+" Called createNextSubContract on main contract to create SC"+sliceIndex)
+  addr=await contracts["main"].createNextSubContract({from: addreses[caller],gas: 2000000})
+  contracts[sliceIndex]=await affiliateSubcontract.at(addr)
+}
+
+
+let index = 0;
+let contracts={}
+let addresses={}
+
+async function nextSlice() {
+  console.log("TIME: Slice " + index + " ended");
+  console.log("TIME: Slice " + (index+1) + " started");
+  console.log("SC" + (index+1) + ": SGP started");
+  setTimeout(function() { console.log("SC" + (index) + ": SGP ended"); }, SGPlength*1000);
+  index += 1;
+  sliceActions(index)
+  
+	if (index <= nSubcontracts) {
+		setTimeout(nextSlice, sliceLength*1000);
+    }
+}
+
+async function sliceActions(slice) {
+  switch(slice){
+    case 0:
+      setTimeout(function() {updateCurrentTotal(0,"Affiliate")  }, (sliceLength/12)*1000);
+      setTimeout(function() {checkEarnings(0,"Affiliate")  }, (2*sliceLength/12)*1000);
+      setTimeout(function() {updateCurrentTotal(0,"Affiliate")  }, (3*sliceLength/12)*1000);
+      setTimeout(function() {checkEarnings(0,"Affiliate")  }, (4*sliceLength/12)*1000);
+      break;
+
+    case 1:
+      setTimeout(function() {updateCurrentTotal(0,"Affiliate")  }, (sliceLength/12)*1000);
+      setTimeout(function() {checkEarnings(0,"Affiliate")  }, (2*sliceLength/12)*1000);
+      setTimeout(function() {updateCurrentTotal(0,"Affiliate")  }, (3*sliceLength/12)*1000);
+      setTimeout(function() {checkEarnings(0,"Affiliate")  }, (4*sliceLength/12)*1000);
+      break;
+
+    case 2:
+      setTimeout(function() {updateCurrentTotal(2,"Affiliate")  }, (sliceLength/12)*1000);
+      setTimeout(function() {checkEarnings(2,"Affiliate")  }, (2*sliceLength/12)*1000);
+      setTimeout(function() {updateCurrentTotal(2,"Affiliate")  }, (3*sliceLength/12)*1000);
+      setTimeout(function() {checkEarnings(2,"Affiliate")  }, (4*sliceLength/12)*1000);
+      break;
+
+    case 3:
+      setTimeout(function() {updateCurrentTotal(3,"Affiliate")  }, (sliceLength/12)*1000);
+      setTimeout(function() {checkEarnings(3,"Affiliate")  }, (2*sliceLength/12)*1000);
+      setTimeout(function() {updateCurrentTotal(3,"Affiliate")  }, (3*sliceLength/12)*1000);
+      setTimeout(function() {checkEarnings(3,"Affiliate")  }, (4*sliceLength/12)*1000);
+      break;
+  }
+}
+
+
+
+
 (async () => {
   
-  const { sc0, mainContract, ownerAddr, affiliateAddr, oracleAddr, web3js } = await init(4, 600, 300, '1', '1', 10)
+  const { sc0, mainContract, ownerAddr, affiliateAddr, oracleAddr, web3js } = await init(nSubcontracts, sliceLength, SGPlength, '1', '1', commission)
+  addresses["Affiliate"]=affiliateAddr
+  addresses["Seller"]=ownerAddr
+  contracts['main']=mainContract
+  contracts['0']=sc0
+
   await mainContract.setOracleAddress(oracleAddr, {from: ownerAddr})
   startTime = await sc0.startTime.call()
+  const now = new Date()  
+  const nowEpoch = (Math.floor(now.getTime()/1000) + now.getTimezoneOffset()*60) 
+  setTimeout(nextSlice, (sliceLength-(nowEpoch-startTime))*1000);
+  sliceActions(0)
+  console.log(now.get)
+  console.log(""+nowEpoch)
+  console.log(""+startTime)
 
-  console.log(startTime.toString())  
-  const gas=await sc0.updateCurrentTotal.estimateGas({from: affiliateAddr})
-  console.log(gas)
-  await sc0.updateCurrentTotal({from: affiliateAddr,gas: 2000000})
+  console.log(((nowEpoch-startTime)).toString())  
+  //const gas=await sc0.updateCurrentTotal.estimateGas({from: affiliateAddr})
+  //console.log(gas)
+  //await sc0.updateCurrentTotal({from: affiliateAddr,gas: 2000000})
 
-  
+
+
   /*setInterval( async () => { 
     await subcontract.methods.updateCurrentTotal().send({ from: accounts[1], gas: 200000 })
   }, SLEEP_INTERVAL);*/
